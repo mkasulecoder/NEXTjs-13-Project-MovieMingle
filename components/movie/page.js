987b00aaa161c9c690c62315bc9159a013130fb7
casulemarc <casulemarc2014@gmail.com>
2023-06-20 20:43:03 -0400

    🧱 Add movie cards, react player, react modal

--- components/movie/page.js
+++ components/movie/page.js
@@ -1,39 +1,77 @@
-import React from "react";
-// import Modal from "react-modal";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
+import ReactPlayer from "react-player";
 
 function AllMovies() {
+  const [movies, setMovies] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    // fetch api
+    fetch("http://localhost:8080/api/v1/movies/")
+      .then((res) => res.json())
+      .then((movie) => setMovies(movie))
+      .catch((error) => {
+        console.error("Failed to fetch data", error);
+      });
+  }, []);
+
+  const handleWatchNow = (movie) => {
+    setSelectedMovie(movie);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setSelectedMovie(null);
+  };
+
   return (
     <div>
       <div className='container movie-section text-white w-100 my-3 w-100'>
-        <div className='movie m-2'>
-          <div>
-            <img src='{image.poster}' alt='Image' />
-          </div>
-          <div className='movie-info'>
-            <h3 className='fw-bold'>Title</h3>
-            <div className='d-flex flex-column justify-content-start align-items-start gap-1'>
-              <span className='orange fw-bold'> genres</span>
-              <span className='green'> Released </span>
+        {/* Loop through all the movies */}
+        {movies.map((movie) => (
+          // movie card
+          <div key={movie._id} className='movie m-2'>
+            <div>
+              <img src={movie.poster} alt='Image' />
             </div>
-          </div>
-          <div className='overview'>
-            <h4>Title</h4>
-            <div className='mb-2'>
-              {/* Map genres */}
-              <span className='mbr-3 green'>genres</span>
+            <div className='movie-info'>
+              <h3 className='fw-bold'>{movie.title}</h3>
+              <div className='d-flex flex-column justify-content-start align-items-start gap-1'>
+                <span className='orange fw-bold'>{movie.genres[0]}</span>
+                <span className='green'> Released {movie.releaseDate}</span>
+              </div>
             </div>
+            <div className='overview'>
+              <h4>{movie.title}</h4>
+              <div className='mb-2'>
+                {/* Map genres */}
+                {movie.genres.map((genre) => (
+                  <span key={genre.id} className='mbr-3 green'>
+                    {genre}{" "}
+                  </span>
+                ))}
+              </div>
 
-            <div className='d-flex flex-row align-items-center justify-content-center'>
-              <button className='btn btn-warning w-100 fw-bold'>
-                {" "}
-                Watch Now
-              </button>
+              <div className='d-flex flex-row align-items-center justify-content-center'>
+                <button
+                  className='btn btn-warning w-100 fw-bold'
+                  onClick={() => handleWatchNow(movie)}
+                >
+                  {" "}
+                  Watch Now
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
 
-      {/* <Modal
+      <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         contentLabel='Video Modal'
@@ -47,11 +85,24 @@ function AllMovies() {
             margin: "auto", // Center the modal horizontally
             padding: "0px",
             border: "none",
+            overflow: "hidden",
           },
         }}
       >
-        <ReactPlayer></ReactPlayer>
-      </Modal> */}
+        {/* selectedMovie = movie */}
+
+        {selectedMovie && (
+          <div>
+            <ReactPlayer
+              url={selectedMovie.trailerLink}
+              height='85vh'
+              width='850px'
+              controls={true}
+              className='bg-dark overflow-hidden'
+            />
+          </div>
+        )}
+      </Modal>
     </div>
   );
 }
